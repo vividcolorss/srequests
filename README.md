1cb1a8e5b502a5b10603d79df9d47bde78f84b5e
paradox89 <72633854+89paradox@users.noreply.github.com>
2022-11-10 15:00:34 -0800

    v0.0.1

---
+++ README.md
@@ -0,0 +1,38 @@
+# srequests
+## A package for taking requests from a Scratch project.
+
+# Documentation
+## For making a project, open this project and backpack the main sprite: https://scratch.mit.edu/projects/759181792/
+
+## Connect to a project
+To connect to a project, import the library and connect to a project with your credentials.
+
+***
+from srequests import srequests
+
+sr = srequests.SRequests("bloxabyte","ws293694","759181792")
+***
+
+## Check if request is unanswered.
+To check if there is an unanswered request, use Request.IsPending().
+
+***
+if sr.Request.IsPending():
+    print("Request unanswered!")
+***
+
+## Respond to a request
+To reply, use the function Request.Respond().
+
+***
+if sr.Request.IsPending():
+    sr.Request.Respond("Response")
+***
+
+## Get request data
+To get the request info & content, use Request.GetName() and Request.GetContent().
+
+***
+name = sr.Request.GetName()
+content = sr.Request.GetContent()
+***
---
+++ src/srequests/srequests.py
@@ -0,0 +1,26 @@
+import scratchattach as scratch3
+from scratchattach import Encoding
+from time import sleep
+class SRequests():
+    def __init__(self,username,password,project_id):
+        session = scratch3.login(username, password)
+        global conn
+        conn = session.connect_cloud(project_id)
+    class Request():
+        def IsPending():
+            responded = conn.get_var("_REQ_RESPONDED")
+            if responded == "1":
+                return False
+            elif responded == "0":
+                return True
+            else:
+                return None
+        def GetName():
+            data = Encoding.decode(conn.get_var("_REQ_NAME"))
+            return data
+        def GetContent():
+            data = Encoding.decode(conn.get_var("_REQ_CONTENT"))
+            return data
+        def Respond(response):
+            conn.set_var("_REQ_RESPONSE", Encoding.encode(response))
+            conn.set_var("_REQ_RESPONDED", "1")
